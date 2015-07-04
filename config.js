37944cedab8d5893369c162769f03ec2421f5dbe
rgpass <rgpass@gmail.com>
2015-07-03 23:02:22 -0400

    Added notes for Foreman

--- config.js
+++ config.js
@@ -1,7 +1,9 @@
 // Access Modulus' Mongo console:
 // mongo apollo.modulusmongo.net:27017/ewU8mapu -u <user> -p <pass>
 module.exports = {
+  // Comes from .env file -- Foreman creates its own, though (5000)
   'port': process.env.PORT || 8080,
-  'database': 'mongodb://rgpass:twolitersaday@apollo.modulusmongo.net:27017/ewU8mapu',
+  // Not used anymore, set for dev in .env and for production on Heroku
+  'database': 'mongodb://rgpass:twolitersaday@apollo.modulusmongo.net:27017/ewU8mapu', 
   'secret': 'twolitersaday'
 };
